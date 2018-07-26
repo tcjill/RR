9a4d2d3352f7221f082054add6c923c43d869e50
jill <jgundaker@gmail.com>
2018-07-26 08:42:32 -0700

    deleted SW and tried first SW script

--- sw.js
+++ sw.js
@@ -1,68 +1,57 @@
-var cacheID = "Restaurant Reviews Cached";
+var staticCacheName = 'restaurant-cahce-1';
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(cacheID).then(cache => {
-      return cache
-        .addAll([
-          "/",
-      "./index.html",
-      "./restaurant.html",
-      "./css/styles.css",
-      "./js/main.js",
-      "./js/restaurant_info.js",
-      "./js/dbhelper.js",
-      "./data/restaurants.json",
-      "./img/1.jpg",
-      "./img/2.jpg",
-      "./img/3.jpg",
-      "./img/4.jpg",
-      "./img/5.jpg",
-      "./img/6.jpg",
-      "./img/7.jpg",
-      "./img/8.jpg",
-      "./img/9.jpg",
-      "./img/10.jpg"
-      ])
-        .catch(error => {
-          console.log("Caches open failed: " + error);
-        });
-    })
-  );
+let urlToCache = [
+    '/',
+    './restaurant.html',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/dbhelper.js',
+
+];
+self.addEventListener('install', function (event) {
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            console.log(cache);
+            return cache.addAll(urlToCache);
+
+        }).catch(erroe => {
+            console.log(erroe);
+        })
+    );
 });
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-      caches.open(staticCacheName).then((cache) => {
-          console.log(cache);
-          return cache.addAll(urlsToCache);
-
-      }).catch((error) => {
-          console.log(error);
-      })
-  );
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-      caches.keys().then((cacheNames) => {
-          return Promise.all(
-              cacheNames.filter((cacheName) => {
-                  return cacheName.startsWith("restaurant-") &&
-                      cacheName != staticCacheName;
-              }).map((cacheName) => {
-                  return caches.delete(cacheName);
-              })
-          );
-      })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-      caches.match(event.request).then((response) => {
-          if (response) return response;
-          return fetch (event.request);
-      })
-  );
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
