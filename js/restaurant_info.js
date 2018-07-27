7d5db310620a95d40532bd866c782288cbb5506e
jill <jgundaker@gmail.com>
2018-07-27 13:35:29 -0700

    set alt for image

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -87,6 +87,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
+  image.setAttribute("alt", restaurant.name + "restaurant");
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
